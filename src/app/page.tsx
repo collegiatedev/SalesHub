f6d5233ff67bb5b79a4f03d125777e876bc1c5a3
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-07 16:12:06 -0400

    bruh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,24 @@
 
 "use client";
 
-import { useEffect, Suspense } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+// import { useEffect, Suspense } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
+  return <div>Home</div>;
+  // const router = useRouter();
+  // const searchParams = useSearchParams();
 
-  // default to c1 form
-  useEffect(() => {
-    const searchParamsString = searchParams.toString();
-    const newUrl = searchParamsString ? `/c1?${searchParamsString}` : "/c1";
-    router.push(newUrl);
-  }, [router, searchParams]);
+  // // default to c1 form
+  // useEffect(() => {
+  //   const searchParamsString = searchParams.toString();
+  //   const newUrl = searchParamsString ? `/c1?${searchParamsString}` : "/c1";
+  //   router.push(newUrl);
+  // }, [router, searchParams]);
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <main />
-    </Suspense>
-  );
+  // return (
+  //   <Suspense fallback={<div>Loading...</div>}>
+  //     <main />
+  //   </Suspense>
+  // );
 }
