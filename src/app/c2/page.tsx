a449d737810e10b96942890a1011661984ae6ae0
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-09 17:38:28 -0400

    c2, c3 boilerplate

---
+++ src/app/c2/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { generateId } from "@/src/utils/id";
+import { CalC1, CalPrefills } from "./_components/cal";
+import { TallyC1 } from "./_components/tally";
+
+export default function C1() {
+  const [prefills, setPrefills] = useState<CalPrefills | null>(null);
+
+  const [id, setId] = useState<string | null>(null);
+  useEffect(() => {
+    setId(generateId());
+  }, []);
+
+  if (!id) return <div>Loading...</div>;
+
+  return (
+    <main>
+      {!prefills ? (
+        <TallyC1 id={id!} setPrefills={setPrefills} />
+      ) : (
+        <CalC1
+          id={prefills.id}
+          name={prefills.name}
+          smsReminderNumber={prefills.smsReminderNumber}
+          email={prefills.email}
+          guests={prefills.guests}
+        />
+      )}
+    </main>
+  );
+}
---
+++ src/app/c3/_components/times.ts
@@ -0,0 +1,6 @@
+// cal_id: c3_link
+export const calIdToLink = {
+  "737019": "https://cal.com/jesse-lee/c3",
+  "908191": "https://cal.com/ibansal/c3",
+  "908187": "https://cal.com/mridulp/c3",
+};
