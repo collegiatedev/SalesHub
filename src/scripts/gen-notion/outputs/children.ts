021cccfa9288cc6a83c71d06f70c376e22d1e2d3
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-13 01:35:54 -0400

    completed recursive output

--- src/scripts/gen-notion/outputs/children.ts
+++ src/scripts/gen-notion/outputs/children.ts
@@ -1,18 +1,31 @@
 import { CHILDREN_DIRECTORY, notion } from "../constants";
 import { createOutput } from "./create";
 
-// remove plain text, href fields from block object
-const removeFields = (obj: any): any => {
-  if (Array.isArray(obj)) {
-    return obj.map(removeFields);
-  } else if (obj && typeof obj === "object") {
-    return Object.fromEntries(
-      Object.entries(obj)
-        .filter(([key]) => key !== "plain_text" && key !== "href")
-        .map(([key, value]) => [key, removeFields(value)])
-    );
-  }
-  return obj;
+const parseBlock = (block: any) => {
+  // remove plain text, href fields from block object
+  const removeFields = (obj: any): any => {
+    if (Array.isArray(obj)) {
+      return obj.map(removeFields);
+    } else if (obj && typeof obj === "object") {
+      return Object.fromEntries(
+        Object.entries(obj)
+          .filter(([key]) => key !== "plain_text" && key !== "href")
+          .map(([key, value]) => [key, removeFields(value)])
+      );
+    }
+    return obj;
+  };
+
+  const type = block.type;
+  // banned block types by notion api
+  if (
+    type === "rollup" ||
+    type === "created_by" ||
+    type === "last_edited_by" ||
+    type === "last_edited_time"
+  )
+    return;
+  return { [type]: removeFields(block[type]) };
 };
 
 export const outputChildren = async (pageId: string) => {
@@ -27,26 +40,48 @@ export const outputChildren = async (pageId: string) => {
   });
 
   const children: any[] = [];
+  page.results.forEach(async (block: any) => {
+    const parsedBlock = parseBlock(block);
+    if (parsedBlock) children.push(parsedBlock);
 
-  page.results.forEach((block: any) => {
-    const type = block.type;
-
-    // banned block types by notion api
-    if (
-      type === "rollup" ||
-      type === "created_by" ||
-      type === "last_edited_by" ||
-      type === "last_edited_time"
-    )
-      return;
-
-    children.push({ [type]: removeFields(block[type]) });
+    await recursiveChildren(
+      block.id,
+      `${CHILDREN_DIRECTORY}${pageId}/`,
+      block.has_children
+    );
   });
-
   await createOutput({
     pageId,
     directory: CHILDREN_DIRECTORY,
     content: children,
     subfolder: true,
+  });
+};
+
+const recursiveChildren = async (
+  blockId: string,
+  directory: string,
+  hasChildren: boolean
+) => {
+  if (!hasChildren) return;
+
+  const blocks = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  const newDirectory = `${directory}${blockId}/`;
+
+  const children: any[] = [];
+  blocks.results.forEach(async (block: any) => {
+    const parsedBlock = parseBlock(block);
+    if (parsedBlock) children.push(parsedBlock);
+    await recursiveChildren(block.id, newDirectory, block.has_children);
+  });
+
+  await createOutput({
+    pageId: blockId,
+    directory: newDirectory,
+    content: children,
+    subfolder: hasChildren,
   });
 };
