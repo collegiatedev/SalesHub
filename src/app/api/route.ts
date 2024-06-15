ef45bc7d4220a15c8c1257244686a18202069f53
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-15 14:13:55 -0400

    testing api stuff

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_req: NextRequest) {
+  console.log("hello world");
+  console.log("bb:", process.env.TEST);
   return NextResponse.json({
     message: `Success!`,
   });
