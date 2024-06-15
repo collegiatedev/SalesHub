053234520432498adda279201e3a5b1379691f9d
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-06-15 18:54:46 -0400

    add tallyp2

---
+++ src/app/c2/_components/tallyp2.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { concentrationToTally } from "./concentrationLinks";
+
+interface TallyC2P2Props {
+  id: string;
+  name: string;
+  grade: string;
+  concentration: string;
+}
+
+const getConcentrationString = (concentrationInput: any) => {
+  if (
+    typeof concentrationInput === "object" &&
+    concentrationInput !== null &&
+    "concentration" in concentrationInput
+  ) {
+    return concentrationInput.concentration;
+  }
+  return concentrationInput; // Return the input directly if it's not an object with a concentration property
+};
+
+export const TallyC2P2 = ({ id, name, concentration }: TallyC2P2Props) => {
+  const concentrationString = getConcentrationString(concentration);
+  const tallyId = concentrationToTally.get(concentrationString.toString());
+
+  if (tallyId === null) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <p>Thank We Will See You Soon! No Further Information Needed</p>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        margin: 0,
+        overflow: "hidden",
+        position: "relative",
+      }}
+    >
+      <iframe
+        src={`https://tally.so/r/${tallyId}?transparentBackground=1&id=${id}&name=${name}`}
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          border: "none",
+        }}
+        title="Student Work Call Registration"
+      ></iframe>
+    </div>
+  );
+};
