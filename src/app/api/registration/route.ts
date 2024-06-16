44891fe1025c397c3240082c7066a2c662ec3cac
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-16 16:33:38 -0400

    promise wrapped await calls

--- src/app/api/registration/route.ts
+++ src/app/api/registration/route.ts
@@ -2,7 +2,9 @@ import { generateConductC1MeetingInDatabase } from "@/src/templates/generateCond
 import { ACCELERATOR_TASKS_DB } from "@/src/utils/constants";
 import { NextRequest, NextResponse } from "next/server";
 
-// only get works with Make for some reason
+// Example: localhost:3000/api/registration?studentId=kz7zh7t2aw&studentFullName=John%20Doe&studentEmail=johndoe@example.com&studentNumber=+19254873772&parentEmail=parent@example.com&parentNumber=+19254873772&repPageId=cece3b6098b3469ebd2261ebd7319aad&leadPageId=3eca26b8664a478a8e7060967dd79c3f&repId=737019&grade=8th
+
+// only GET works with Make.com for some reason
 export async function GET(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
@@ -18,37 +20,40 @@ export async function GET(req: NextRequest) {
     const repId = searchParams.get("repId");
     const grade = searchParams.get("grade");
 
-    if (
-      !studentId ||
-      !studentFullName ||
-      !studentEmail ||
-      !studentNumber ||
-      !parentEmail ||
-      !parentNumber ||
-      !repPageId ||
-      !leadPageId ||
-      !repId ||
-      !grade
-    ) {
+    const missingParams = [];
+    if (!studentId) missingParams.push("studentId");
+    if (!studentFullName) missingParams.push("studentFullName");
+    if (!studentEmail) missingParams.push("studentEmail");
+    if (!studentNumber) missingParams.push("studentNumber");
+    if (!parentEmail) missingParams.push("parentEmail");
+    if (!parentNumber) missingParams.push("parentNumber");
+    if (!repPageId) missingParams.push("repPageId");
+    if (!leadPageId) missingParams.push("leadPageId");
+    if (!repId) missingParams.push("repId");
+    if (!grade) missingParams.push("grade");
+
+    if (missingParams.length > 0) {
       return NextResponse.json(
         {
-          message: "Missing required parameters",
+          message: `Missing required parameters: ${missingParams.join(", ")}`,
         },
-        { status: 400 }
+        {
+          status: 400,
+        }
       );
     }
 
     await generateConductC1MeetingInDatabase({
-      studentId,
-      studentFullName,
-      studentEmail,
-      studentNumber,
-      parentEmail,
-      parentNumber,
-      repPageId,
-      leadPageId,
-      repId,
-      grade,
+      studentId: studentId as string,
+      studentFullName: studentFullName as string,
+      studentEmail: studentEmail as string,
+      studentNumber: studentNumber as string,
+      parentEmail: parentEmail as string,
+      parentNumber: parentNumber as string,
+      repPageId: repPageId as string,
+      leadPageId: leadPageId as string,
+      repId: repId as string,
+      grade: grade as string,
       parentId: ACCELERATOR_TASKS_DB,
     });
 
