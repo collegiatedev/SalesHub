ee12f81b87e3ab466e5b1d6bc7fde48a0aa85e41
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-15 20:10:30 -0400

    testing search params

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(_req: NextRequest) {
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
   return NextResponse.json({
-    message: `Success Here!`,
+    message: `Success Here! ${searchParams.get("name")}`,
   });
 }
