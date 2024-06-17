c00b9b46b1618c3995d35eda39d329d554cb064f
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-17 14:26:16 -0400

    refractored all endpoints

--- dist/routes/registration.js
+++ dist/routes/registration.js
@@ -3,61 +3,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.registrationRouter = void 0;
 const express_1 = require("express");
 const generateConductC1MeetingInDatabase_1 = require("../templates/generateConductC1MeetingInDatabase");
+const routers_1 = require("../utils/routers");
 const constants_1 = require("../utils/constants");
 exports.registrationRouter = (0, express_1.Router)();
 // example url: http://localhost:8080/registration?studentId=kz7zh7t2aw&studentFullName=John%20Doe&studentEmail=johndoe@example.com&studentNumber=+19254873772&parentEmail=parent@example.com&parentNumber=+19254873772&repPageId=cece3b6098b3469ebd2261ebd7319aad&leadPageId=3eca26b8664a478a8e7060967dd79c3f&repId=737019&grade=8th
 // For some reason, need GET for Make.com to behave
-exports.registrationRouter.get("/", async (req, res) => {
-    try {
-        const { studentId, studentFullName, studentEmail, studentNumber, parentEmail, parentNumber, repPageId, leadPageId, repId, grade, } = req.query;
-        const missingParams = [];
-        if (!studentId)
-            missingParams.push("studentId");
-        if (!studentFullName)
-            missingParams.push("studentFullName");
-        if (!studentEmail)
-            missingParams.push("studentEmail");
-        if (!studentNumber)
-            missingParams.push("studentNumber");
-        if (!parentEmail)
-            missingParams.push("parentEmail");
-        if (!parentNumber)
-            missingParams.push("parentNumber");
-        if (!repPageId)
-            missingParams.push("repPageId");
-        if (!leadPageId)
-            missingParams.push("leadPageId");
-        if (!repId)
-            missingParams.push("repId");
-        if (!grade)
-            missingParams.push("grade");
-        if (missingParams.length > 0) {
-            return res.status(400).json({
-                message: `Missing required parameters: ${missingParams.join(", ")}`,
-            });
-        }
-        await (0, generateConductC1MeetingInDatabase_1.generateConductC1MeetingInDatabase)({
-            studentId: studentId,
-            studentFullName: studentFullName,
-            studentEmail: studentEmail,
-            studentNumber: studentNumber,
-            parentEmail: parentEmail,
-            parentNumber: parentNumber,
-            repPageId: repPageId,
-            leadPageId: leadPageId,
-            repId: repId,
-            grade: grade,
-            parentId: constants_1.ACCELERATOR_TASKS_DB,
+exports.registrationRouter.get("/", (0, routers_1.asyncHandler)(async (req, res) => {
+    const validatedParams = (0, routers_1.checkQueryParams)(req, [
+        "studentId",
+        "studentFullName",
+        "studentEmail",
+        "studentNumber",
+        "parentEmail",
+        "parentNumber",
+        "repPageId",
+        "leadPageId",
+        "repId",
+        "grade",
+    ]);
+    if (!validatedParams.isValid)
+        return res.status(400).json({
+            message: validatedParams.error,
         });
-        return res.json({
-            message: "task created",
-        });
-    }
-    catch (error) {
-        // Handle any errors that occur
-        return res.status(500).json({
-            message: "Failed to create meeting",
-            error,
-        });
-    }
-});
+    await (0, generateConductC1MeetingInDatabase_1.generateConductC1MeetingInDatabase)({
+        ...validatedParams.params,
+        parentId: constants_1.ACCELERATOR_TASKS_DB,
+    });
+    return res.json({
+        message: "task created",
+    });
+}));
--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -1,66 +1,44 @@
 import { Router, Request, Response } from "express";
-import { generateConductC1MeetingInDatabase } from "../templates/generateConductC1MeetingInDatabase";
+import {
+  generateConductC1MeetingInDatabase,
+  GenerateConductC1MeetingInDatabaseProps,
+} from "../templates/generateConductC1MeetingInDatabase";
+import { asyncHandler, checkQueryParams } from "../utils/routers";
 import { ACCELERATOR_TASKS_DB } from "../utils/constants";
 
 export const registrationRouter: Router = Router();
 
 // example url: http://localhost:8080/registration?studentId=kz7zh7t2aw&studentFullName=John%20Doe&studentEmail=johndoe@example.com&studentNumber=+19254873772&parentEmail=parent@example.com&parentNumber=+19254873772&repPageId=cece3b6098b3469ebd2261ebd7319aad&leadPageId=3eca26b8664a478a8e7060967dd79c3f&repId=737019&grade=8th
 // For some reason, need GET for Make.com to behave
-registrationRouter.get("/", async (req: Request, res: Response) => {
-  try {
-    const {
-      studentId,
-      studentFullName,
-      studentEmail,
-      studentNumber,
-      parentEmail,
-      parentNumber,
-      repPageId,
-      leadPageId,
-      repId,
-      grade,
-    } = req.query;
+registrationRouter.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response) => {
+    const validatedParams =
+      checkQueryParams<GenerateConductC1MeetingInDatabaseProps>(req, [
+        "studentId",
+        "studentFullName",
+        "studentEmail",
+        "studentNumber",
+        "parentEmail",
+        "parentNumber",
+        "repPageId",
+        "leadPageId",
+        "repId",
+        "grade",
+      ]);
 
-    const missingParams = [];
-    if (!studentId) missingParams.push("studentId");
-    if (!studentFullName) missingParams.push("studentFullName");
-    if (!studentEmail) missingParams.push("studentEmail");
-    if (!studentNumber) missingParams.push("studentNumber");
-    if (!parentEmail) missingParams.push("parentEmail");
-    if (!parentNumber) missingParams.push("parentNumber");
-    if (!repPageId) missingParams.push("repPageId");
-    if (!leadPageId) missingParams.push("leadPageId");
-    if (!repId) missingParams.push("repId");
-    if (!grade) missingParams.push("grade");
-
-    if (missingParams.length > 0) {
+    if (!validatedParams.isValid)
       return res.status(400).json({
-        message: `Missing required parameters: ${missingParams.join(", ")}`,
+        message: validatedParams.error,
       });
-    }
 
     await generateConductC1MeetingInDatabase({
-      studentId: studentId as string,
-      studentFullName: studentFullName as string,
-      studentEmail: studentEmail as string,
-      studentNumber: studentNumber as string,
-      parentEmail: parentEmail as string,
-      parentNumber: parentNumber as string,
-      repPageId: repPageId as string,
-      leadPageId: leadPageId as string,
-      repId: repId as string,
-      grade: grade as string,
-      parentId: ACCELERATOR_TASKS_DB as string,
+      ...validatedParams.params,
+      parentId: ACCELERATOR_TASKS_DB,
     });
 
     return res.json({
       message: "task created",
     });
-  } catch (error) {
-    // Handle any errors that occur
-    return res.status(500).json({
-      message: "Failed to create meeting",
-      error,
-    });
-  }
-});
+  })
+);
