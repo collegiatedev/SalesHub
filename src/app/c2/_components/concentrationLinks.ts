9ffa9d2277e1cf3bda5ef361f3abd27ccfc909c1
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2024-06-19 21:06:28 -0400

    change the id based on stage

--- src/app/c2/_components/concentrationLinks.ts
+++ src/app/c2/_components/concentrationLinks.ts
@@ -16,9 +16,9 @@ export const concentrationToTally = new Map<string, string | null>([
 ]);
 
 export const concentrationToNotion = new Map<string, string>([
-  ["Personal Branding", "2f2d59112155431b87952c2111e57367"],
-  ["EC Discovery", "a9dbeaeef7da4d27ab347fa462e9c755"],
-  ["Org Optimization", "8b4ca1c9940346e1b0b8fd38f38bbdc7"],
-  ["Internship Search", "02fefa60e36e4329b72167aebc828cd2"],
-  ["Essay Editing", "be10623d03104c2095b196c4ac2a95d8"],
+  ["Personal Branding", "C2-PB"],
+  ["EC Discovery", "C2-ECD"],
+  ["Org Optimization", "C2-OO"],
+  ["Internship Search", "C2-IS"],
+  ["Essay Editing", "C2-EE"],
 ]);
