a3ef26298b98b57855c36ba19fe608c4489d8d02
Jesse Lee <the.jesse.dev@gmail.com>
2024-06-30 18:06:11 -0400

    removed more unneeded

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,9 +5,3 @@ export const CONDUCT_C2_MEETING_PB_TASK = "29d8e847ce87449988c992015b46a7a6";
 export const CREATE_STUDENT_DASHBOARD_TASK = "c735d37a78cd43a28423a7caf9b37ab9";
 export const GIVE_ADDITIONAL_FEEDBACK_TO_PB_TASK =
   "63c03a42c94d4d38b30835434dd22e01";
-
-export enum FileExtension {
-  PDF = ".pdf",
-  DOC = ".doc",
-  DOCX = ".docx",
-}
