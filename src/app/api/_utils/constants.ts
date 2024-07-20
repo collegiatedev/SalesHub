0c3d9ff4d626c4003a174b59df698a6c8c8b96c0
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-20 19:18:34 -0400

    env config url changes

--- src/app/api/_utils/constants.ts
+++ src/app/api/_utils/constants.ts
@@ -8,6 +8,10 @@ export const LEAD_DATABASE_ID = "27386326248f4dae9374811627be3036";
 
 // express server setup
 export const SERVER_URL = "https://king-prawn-app-onivj.ondigitalocean.app";
+// next url
+const isProduction = process.env.VERCEL_ENV === "production";
+const deployedURL = `https://${process.env.VERCEL_URL}`;
+const NEXT_URL = isProduction ? deployedURL : "http://localhost:3000"; // might be 3001 sometimes
 
 // google auth setup
 import { google } from "googleapis";
@@ -16,7 +20,7 @@ import * as path from "path";
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `${process.env.NEXTAUTH_URL}/api/auth/callback/`
+  `${NEXT_URL}/api/auth/callback/`
 );
 export const TOKEN_PATH = path.join(process.cwd(), "token.json");
 
