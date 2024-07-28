77a54870b3fb8228db84d2ec8105c2549a1b3e68
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-28 13:56:09 -0400

    pls fix

--- forms/src/app/api/_utils/generator/c1tasks.ts
+++ forms/src/app/api/_utils/generator/c1tasks.ts
@@ -1,6 +1,73 @@
 import axios from "axios";
 import { SERVER_URL } from "../../constants";
 import { LeadFields } from "../notion/getLead";
+// I can change this probably to one interface and then use partials?
+interface conductC1Info {
+  studentName: string;
+  studentPageId: string;
+  repPageId: string;
+  time: string;
+  studentId: string;
+  studentEmail: string;
+  studentNumber: string;
+  parentEmail: string;
+  parentNumber: string;
+}
+const conductC1Task = async (conductC1Info: conductC1Info) => {
+  try {
+    const response = await axios.post(
+      `${SERVER_URL}/c1/conduct`,
+      conductC1Info
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error creating info:", error);
+    throw error;
+  }
+};
+
+interface createDashboardInfo {
+  studentName: string;
+  studentPageId: string;
+  repPageId: string;
+  time: string;
+  folderLink: string;
+  studentEmail: string;
+}
+const createDashboardTask = async (
+  createDashboardInfo: createDashboardInfo
+) => {
+  try {
+    const response = await axios.post(
+      `${SERVER_URL}/c1/dashboard`,
+      createDashboardInfo
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error creating info:", error);
+    throw error;
+  }
+};
+
+interface createGCInfo {
+  studentName: string;
+  studentPageId: string;
+  repPageId: string;
+  time: string;
+  studentId: string;
+  parentName: string;
+  studentPhone: string;
+  parentPhone: string;
+}
+const createGCTask = async (createGCInfo: createGCInfo) => {
+  try {
+    const response = await axios.post(`${SERVER_URL}/c1/gc`, createGCInfo);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating info:", error);
+    throw error;
+  }
+};
 
 interface C1TaskFields {
   lead: LeadFields;
@@ -8,7 +75,7 @@ interface C1TaskFields {
   repPageId: string;
   folderLink: string;
 }
-export const createTasks = async ({
+const createTasks = async ({
   lead,
   calStartTime,
   repPageId,
@@ -47,70 +114,4 @@ export const createTasks = async ({
   ]);
 };
 
-// I can change this probably to one interface and then use partials?
-interface conductC1Info {
-  studentName: string;
-  studentPageId: string;
-  repPageId: string;
-  time: string;
-  studentId: string;
-  studentEmail: string;
-  studentNumber: string;
-  parentEmail: string;
-  parentNumber: string;
-}
-export const conductC1Task = async (conductC1Info: conductC1Info) => {
-  try {
-    const response = await axios.post(
-      `${SERVER_URL}/c1/conduct`,
-      conductC1Info
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error creating info:", error);
-    throw error;
-  }
-};
-
-interface createDashboardInfo {
-  studentName: string;
-  studentPageId: string;
-  repPageId: string;
-  time: string;
-  folderLink: string;
-  studentEmail: string;
-}
-export const createDashboardTask = async (
-  createDashboardInfo: createDashboardInfo
-) => {
-  try {
-    const response = await axios.post(
-      `${SERVER_URL}/c1/dashboard`,
-      createDashboardInfo
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Error creating info:", error);
-    throw error;
-  }
-};
-
-interface createGCInfo {
-  studentName: string;
-  studentPageId: string;
-  repPageId: string;
-  time: string;
-  studentId: string;
-  parentName: string;
-  studentPhone: string;
-  parentPhone: string;
-}
-export const createGCTask = async (createGCInfo: createGCInfo) => {
-  try {
-    const response = await axios.post(`${SERVER_URL}/c1/gc`, createGCInfo);
-    return response.data;
-  } catch (error) {
-    console.error("Error creating info:", error);
-    throw error;
-  }
-};
+export default createTasks;
