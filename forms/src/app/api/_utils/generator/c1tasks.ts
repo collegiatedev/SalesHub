3da34da69d76ae4f3d8bf22454429110909b605f
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-28 14:12:17 -0400

    Co-authored-by: Ishaan Bansal <bshaan77@users.noreply.github.com>

--- forms/src/app/api/_utils/generator/c1tasks.ts
+++ forms/src/app/api/_utils/generator/c1tasks.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { SERVER_URL } from "../../constants";
-import { type LeadFields } from "../notion/getLead";
+
 // I can change this probably to one interface and then use partials?
 interface conductC1Info {
   studentName: string;
@@ -13,7 +13,28 @@ interface conductC1Info {
   parentEmail: string;
   parentNumber: string;
 }
-const conductC1Task = async (conductC1Info: conductC1Info) => {
+
+interface createDashboardInfo {
+  studentName: string;
+  studentPageId: string;
+  repPageId: string;
+  time: string;
+  folderLink: string;
+  studentEmail: string;
+}
+
+interface createGCInfo {
+  studentName: string;
+  studentPageId: string;
+  repPageId: string;
+  time: string;
+  studentId: string;
+  parentName: string;
+  studentPhone: string;
+  parentPhone: string;
+}
+
+export const conductC1Task = async (conductC1Info: conductC1Info) => {
   try {
     const response = await axios.post(
       `${SERVER_URL}/c1/conduct`,
@@ -26,15 +47,7 @@ const conductC1Task = async (conductC1Info: conductC1Info) => {
   }
 };
 
-interface createDashboardInfo {
-  studentName: string;
-  studentPageId: string;
-  repPageId: string;
-  time: string;
-  folderLink: string;
-  studentEmail: string;
-}
-const createDashboardTask = async (
+export const createDashboardTask = async (
   createDashboardInfo: createDashboardInfo
 ) => {
   try {
@@ -49,17 +62,7 @@ const createDashboardTask = async (
   }
 };
 
-interface createGCInfo {
-  studentName: string;
-  studentPageId: string;
-  repPageId: string;
-  time: string;
-  studentId: string;
-  parentName: string;
-  studentPhone: string;
-  parentPhone: string;
-}
-const createGCTask = async (createGCInfo: createGCInfo) => {
+export const createGCTask = async (createGCInfo: createGCInfo) => {
   try {
     const response = await axios.post(`${SERVER_URL}/c1/gc`, createGCInfo);
     return response.data;
@@ -67,49 +70,4 @@ const createGCTask = async (createGCInfo: createGCInfo) => {
     console.error("Error creating info:", error);
     throw error;
   }
-};
-
-interface C1TaskFields {
-  lead: LeadFields;
-  calStartTime: string;
-  repPageId: string;
-  folderLink: string;
-}
-export const createC1Tasks = async ({
-  lead,
-  calStartTime,
-  repPageId,
-  folderLink,
-}: C1TaskFields) => {
-  await Promise.all([
-    conductC1Task({
-      studentName: lead.name,
-      studentPageId: lead.pageId,
-      repPageId: repPageId,
-      time: calStartTime,
-      studentId: lead.id,
-      studentEmail: lead.contact.studentEmail,
-      studentNumber: lead.contact.studentPhone,
-      parentEmail: lead.contact.parentEmail,
-      parentNumber: lead.contact.parentPhone,
-    }),
-    createDashboardTask({
-      folderLink,
-      studentName: lead.name,
-      studentPageId: lead.pageId,
-      repPageId: repPageId,
-      time: calStartTime,
-      studentEmail: lead.contact.studentEmail,
-    }),
-    createGCTask({
-      studentName: lead.name,
-      studentPageId: lead.pageId,
-      repPageId: repPageId,
-      time: calStartTime,
-      studentId: lead.id,
-      parentName: lead.contact.parentName,
-      studentPhone: lead.contact.studentPhone,
-      parentPhone: lead.contact.parentPhone,
-    }),
-  ]);
 };
