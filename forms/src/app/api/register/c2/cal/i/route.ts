8cf04732b0c8f8a4d98d48c9f1f492afafefad8d
Jesse Lee <the.jesse.dev@gmail.com>
2024-07-29 17:50:44 -0400

    i/o endpoint boilerplates

---
+++ forms/src/app/api/register/c2/cal/i/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest } from "next/server";
+import { SignatureTypes, webhookHandler } from "../../../../_handlers/webhook";
+import { NEXT_URL, qstashClient } from "~/app/api/constants";
+import { withEndpoint } from "~/app/api/helpers";
+
+export const POST = webhookHandler<any>({
+  type: SignatureTypes.Cal,
+  required: { body: ["payload"] },
+  handler: async (utilContext: any, _req: NextRequest) => {
+    const { payload } = utilContext;
+    return await qstashClient.publishJSON({
+      url: withEndpoint("/api/register/c2/cal/o", NEXT_URL),
+      body: { payload, secret: process.env.INTERNAL_SECRET },
+    });
+  },
+});
