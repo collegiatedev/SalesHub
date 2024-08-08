7e006b180f47f96d025273eae866d5957d29d4e9
Jesse Lee <the.jesse.dev@gmail.com>
2024-08-08 13:07:53 -0400

    fixed rendering bug

--- forms/src/app/essay/cart/_manage/questions.tsx
+++ forms/src/app/essay/cart/_manage/questions.tsx
@@ -15,45 +15,53 @@ export const ManageQuestions = ({
 }: {
   form: UseFormReturn<DraftFormValues>;
 }) => {
-  interface TextAreaFieldProps {
-    name: keyof DraftFormValues;
-    label: string;
-    placeholder?: string;
-  }
-  const TextAreaField = ({ name, label, placeholder }: TextAreaFieldProps) => {
-    return (
-      <FormField
-        control={form.control}
-        name={name}
-        render={({ field }) => (
-          <FormItem className="w-full">
-            <FormLabel>{label}</FormLabel>
-            <FormControl>
-              <Textarea {...field} placeholder={placeholder} />
-            </FormControl>
-          </FormItem>
-        )}
-      />
-    );
-  };
-
   return (
     <CardContent className="space-y-2">
       <TextAreaField
+        form={form}
         name="prompt"
         label="Prompt"
         placeholder="Paste the essay prompt here."
       />
       <TextAreaField
+        form={form}
         name="submission"
         label="Submission"
         placeholder="Paste your essay here."
       />
       <TextAreaField
+        form={form}
         name="notes"
         label="Notes"
         placeholder="Specify if there are any aspects you'd like help on."
       />
     </CardContent>
+  );
+};
+interface TextAreaFieldProps {
+  form: UseFormReturn<DraftFormValues>;
+  name: keyof DraftFormValues;
+  label: string;
+  placeholder?: string;
+}
+const TextAreaField = ({
+  form,
+  name,
+  label,
+  placeholder,
+}: TextAreaFieldProps) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="w-full">
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Textarea {...field} placeholder={placeholder} />
+          </FormControl>
+        </FormItem>
+      )}
+    />
   );
 };
