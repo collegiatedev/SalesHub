f3fa2db07a2dadf44ed5ca86776e125cb76e67b7
Jesse Lee <the.jesse.dev@gmail.com>
2024-08-13 00:49:57 -0400

    reverting back session setup

--- forms/src/app/essay/statuses.tsx
+++ forms/src/app/essay/statuses.tsx
@@ -1,34 +1,19 @@
-// "use client";
+"use client";
 
+import { NEXT_URL } from "../constants";
+import { useRouter, useSearchParams } from "next/navigation";
 import { successfulToast } from "~/components/myToast";
-import { SearchParams, SESSION_QUERY_KEY } from "../constants";
-import { getSessionId } from "./helpers";
 
 // handles query params for successful purchase and other status messages
 // todo, treat component as general alert component for form status
-interface StatusesProps {
-  searchParams: SearchParams;
-}
-export const Statuses = async ({ searchParams }: StatusesProps) => {
-  const success = searchParams["success"];
-  if (success) await handleSuccess(searchParams);
+export const HandleStatuses = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  if (searchParams.has("success")) {
+    successfulToast("Congratulations! Everything has been submitted.");
+    router.replace(`${NEXT_URL}/essay/`);
+  }
 
   return null;
-};
-
-const handleSuccess = async (searchParams: SearchParams) => {
-  const sessionId = getSessionId(searchParams);
-  if (!sessionId) return;
-
-  successfulToast("Congratulations! Everything has been submitted.");
-
-  return;
-
-  // Refresh the page to remaining parameters
-  // const newParams = new URLSearchParams(params.toString());
-  // newParams.delete("success");
-  // newParams.delete(SESSION_QUERY_KEY);
-  // router.replace(`${window.location.pathname}?${newParams.toString()}`, {
-  //   scroll: false,
-  // });
 };
